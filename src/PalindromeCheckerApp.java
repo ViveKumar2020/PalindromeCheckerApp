b55c02980adbaad0669b560aa03e38a2b2bbf86b
ViveKumar2020 <vp6068@srmist.edu.in>
2026-02-23 10:43:13 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,15 +5,12 @@ public class PalindromeCheckerApp {
     public static void main() {
         String original = "madam";
 
-        // Variable to store reversed string
         String reversed = "";
 
-        // Reverse using for loop
         for (int i = original.length() - 1; i >= 0; i--) {
             reversed = reversed + original.charAt(i);
         }
 
-        // Compare original and reversed
         if (original.equals(reversed)) {
             System.out.println(original + " is a Palindrome.");
         } else {
